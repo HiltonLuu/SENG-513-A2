f3a571ac8227c3daa8864f86a2758ce712251b9d
HiltonLuu <60626050+HiltonLuu@users.noreply.github.com>
2023-10-12 23:05:09 -0600

    driver placeholder file and added comments to all files

---
+++ scripts/GameDriver.js
@@ -0,0 +1,10 @@
+/*
+Course: SENG 513
+Date: Oct 12, 2023
+Assignment 2 Part One
+Name: Hilton Luu
+UCID: 30085893
+*/
+
+//flow chart can be found in readme called MatchStart
+const matchStart = () => {}
--- scripts/Knight.js
+++ scripts/Knight.js
@@ -1,3 +1,11 @@
+/*
+Course: SENG 513
+Date: Oct 12, 2023
+Assignment 2 Part One
+Name: Hilton Luu
+UCID: 30085893
+*/
+
 class Knight extends Unit {
     constructor() {
         super({
